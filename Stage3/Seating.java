18bc75c0e4d3c85d2efe587953a8665b79c10ad1
AbstractHaderach <andrew.w.the.cole@gmail.com>
2025-04-13 14:01:13 -0600

    Update Seating.java
    Modified JavaDoc and formatting

--- Stage3/Seating.java
+++ Stage3/Seating.java
@@ -1,11 +1,9 @@
-
 import java.util.Arrays;
-
-
 
 /**
  * This class describes a 2D seating array. Seats are unoccupied if FALSE,
  * occupied if TRUE.
+ * 
  * @author Andrew
  */
 public class Seating {
@@ -13,15 +11,28 @@ public class Seating {
     private final int COL = 10;
     private boolean[][] seatingChart = new boolean[ROW][COL];
     
-    
+    /**
+     * This method 'claims' a seat as occupied.
+     * @param i the seat's row
+     * @param j the seat's column
+     */
     public void occupySeat(int i, int j) {
         seatingChart[i][j] = true;
     }
     
+    /**
+     * This method designates a seat as unoccupied.
+     * @param i the seat's row
+     * @param j the seat's column
+     */
     public void vacateSeat(int i, int j) {
         seatingChart[i][j] = false;
     }
     
+    /**
+     * Prints a readable representation of the seating array. Unoccupied seats
+     * are represented with a "-" while occupied seats are shown as "X".
+     */
     public void showSeats() {
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
@@ -35,6 +46,11 @@ public class Seating {
         }
     }
     
+    /**
+     * @param i the seat row
+     * @param j the seat column
+     * @return whether or not a seat is occupied
+     */
     public boolean getSeat(int i, int j) {
         return seatingChart[i][j];
     }
