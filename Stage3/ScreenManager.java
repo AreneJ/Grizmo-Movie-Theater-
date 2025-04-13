ba79f1b31758a91843e3af834c1ab63425621fa2
AbstractHaderach <andrew.w.the.cole@gmail.com>
2025-04-13 14:00:14 -0600

    Update ScreenManager.java
    Modified JavaDoc and formatting

--- Stage3/ScreenManager.java
+++ Stage3/ScreenManager.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+
 /**
- * This class allows the creation and retrieval of Screen objects
+ * This class creates initial Screen objects and allows for their retrieval.
+ * It also holds the ShowtimeManager object with a corresponding getter.
+ * 
  * @author Andrew
  */
 public class ScreenManager {
@@ -13,33 +16,50 @@ public class ScreenManager {
     private static ShowtimeManager showtimeManager = new ShowtimeManager();
     
     /**
-     * empty/default constructor. ScreenManager simply facilitates the creation
-     * and retrieval of each day's Screen object.
+     * Empty/default constructor
      */
     ScreenManager() {
         
     }
     
+    /**
+     * @return Monday's Screen object
+     */
     public Screen getMScreen() {
         return mScreen;
     }
 
+    /**
+     * @return Tuesday's Screen object
+     */
     public Screen getTScreen() {
         return tScreen;
     }
 
+    /**
+     * @return Wednesday's Screen object
+     */
     public Screen getWScreen() {
         return wScreen;
     }
 
+    /**
+     * @return Thursday's Screen object
+     */
     public Screen getThScreen() {
         return thScreen;
     }
 
+    /**
+     * @return Friday's Screen object
+     */
     public Screen getFScreen() {
         return fScreen;
     }
 
+    /**
+     * @return ShowtimeManager object
+     */
     public ShowtimeManager getShowtimeManager() {
         return showtimeManager;
     }
